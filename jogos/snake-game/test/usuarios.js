cb5a9604a1f8ce13cf0c50873608c5c4e47c79af
dev.teuz <matheus.diegues@outlook.com>
2023-09-03 19:24:31 -0300

    armazenamento de usuarios

--- jogos/snake-game/test/usuarios.js
+++ jogos/snake-game/test/usuarios.js
@@ -1,25 +1,41 @@
 function cadastrarUsuario() {
     const username = prompt('Insira um nome de usuário:');
-    localStorage.setItem('username', username);
+    // Obtém a lista de usuários do localStorage, se existir
+    const userListString = localStorage.getItem('userList');
+    let userList = userListString ? JSON.parse(userListString) : [];
+
+    // Verifica se o usuário já existe
+    if (userList.find(user => user === username)) {
+        alert('Usuário já existe!');
+        return;
+    }
+
+    // Adiciona o novo usuário à lista
+    userList.push(username);
+
+    // Armazena a lista atualizada no localStorage
+    localStorage.setItem('userList', JSON.stringify(userList));
+
     alert('Cadastro realizado com sucesso!');
 }
 
 function fazerLogin() {
     const username = prompt('Insira seu nome de usuário:');
-    const savedUsername = localStorage.getItem('username');
-    if (username === savedUsername) {
+    const userListString = localStorage.getItem('userList');
+    const userList = userListString ? JSON.parse(userListString) : [];
+
+    if (userList.includes(username)) {
         alert('Login realizado com sucesso!');
-        exibirNomeJogadorLogado();
+        exibirNomeJogadorLogado(username);
     } else {
         alert('Nome de usuário inválido!');
     }
 }
 
-function exibirNomeJogadorLogado() {
-    const username = localStorage.getItem('username');
+function exibirNomeJogadorLogado(username) {
     const nomeJogador = document.getElementById('nomeJogador');
     if (username) {
-        nomeJogador.textContent = 'Jogador: ' + username;
+        nomeJogador.textContent = 'Nome do Jogador: ' + username;
     } else {
         nomeJogador.textContent = '';
     }
