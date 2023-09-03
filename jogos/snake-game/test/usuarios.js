9844c7cd2bb97c0755c169e09ba90f9ae1574a76
dev.teuz <matheus.diegues@outlook.com>
2023-09-03 19:11:59 -0300

    add login

---
+++ jogos/snake-game/test/usuarios.js
@@ -0,0 +1,26 @@
+function cadastrarUsuario() {
+    const username = prompt('Insira um nome de usuário:');
+    localStorage.setItem('username', username);
+    alert('Cadastro realizado com sucesso!');
+}
+
+function fazerLogin() {
+    const username = prompt('Insira seu nome de usuário:');
+    const savedUsername = localStorage.getItem('username');
+    if (username === savedUsername) {
+        alert('Login realizado com sucesso!');
+        exibirNomeJogadorLogado();
+    } else {
+        alert('Nome de usuário inválido!');
+    }
+}
+
+function exibirNomeJogadorLogado() {
+    const username = localStorage.getItem('username');
+    const nomeJogador = document.getElementById('nomeJogador');
+    if (username) {
+        nomeJogador.textContent = 'Nome do Jogador: ' + username;
+    } else {
+        nomeJogador.textContent = '';
+    }
+}
